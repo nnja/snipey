1207176398935e71113c83a2b15ae19738e7114e
Wise Guy <nzakharenko@gmail.com>
2013-08-05 13:04:08 -0400

    move urls to config file

--- config.py
+++ config.py
@@ -2,12 +2,19 @@ import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///%s' % os.path.join(BASEDIR, 'app.db'))
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///%s' %
+                                    os.path.join(BASEDIR, 'app.db'))
 
 BOOTSTRAP_USE_MINIFIED = True
 BOOTSTRAP_USE_CDN = False
 BOOTSTRAP_FONTAWESOME = True
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'devkey')
+
+# Meetup Specific Settings
+BASE_URL = 'https://api.meetup.com/2/'
+REQUEST_TOKEN_URL = 'https://api.meetup.com/oauth/request'
+ACCESS_TOKEN_URL = 'https://api.meetup.com/oauth/access'
+AUTHORIZE_URL = 'http://www.meetup.com/authorize'
 CONSUMER_KEY = os.getenv('CONSUMER_KEY', 'your consumer key')
 CONSUMER_SECRET = os.getenv('CONSUMER_SECRET', 'your consumer secret')
--- snipey/__init__.py
+++ snipey/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_oauth import OAuth
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+import config
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,11 +13,11 @@ Bootstrap(app)
 
 oauth = OAuth()
 meetup_oauth = oauth.remote_app('meetup',
-                                base_url='https://api.meetup.com/2/',
-                                request_token_url='https://api.meetup.com/oauth/request',
-                                access_token_url='https://api.meetup.com/oauth/access',
-                                authorize_url='http://www.meetup.com/authorize',
-                                consumer_key=app.config['CONSUMER_KEY'],
-                                consumer_secret=app.config['CONSUMER_SECRET'])
+                                base_url=config.BASE_URL,
+                                request_token_url=config.REQUEST_TOKEN_URL,
+                                access_token_url=config.ACCESS_TOKEN_URL,
+                                authorize_url=config.AUTHORIZE_URL,
+                                consumer_key=config.CONSUMER_KEY,
+                                consumer_secret=config.CONSUMER_SECRET)
 
 from snipey import view
